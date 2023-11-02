9bf035c83b87a219384f2dc6fb86acacd8d5e01b
Thiedze <github@thiems.eu>
2023-11-03 00:03:36 +0100

    change to pi zero

--- python/main.py
+++ python/main.py
@@ -16,27 +16,27 @@ def get_status():
 def update_led_stripe():
     fade_in_counter = 1
     fade_in_direction = 1
+    brightness = 80
     while True:
-        if (gitlab_service.status == Status.CREATED or gitlab_service.status == Status.RUNNING
-                or gitlab_service.status == Status.SCHEDULED):
-            sleep_timer = 0.0025
-            if fade_in_counter >= 255 or fade_in_counter <= 0:
+        if gitlab_service.status in [Status.CREATED, Status.RUNNING, Status.SCHEDULED]:
+            sleep_timer = 0.04
+            if fade_in_counter >= brightness or fade_in_counter <= 0:
                 fade_in_direction *= -1
             fade_in_counter += 1 * fade_in_direction
 
             led_stripe_service.pixels.fill([0, 0, fade_in_counter])
         elif gitlab_service.status == Status.SUCCESS:
             sleep_timer = 10
-            led_stripe_service.pixels.fill([0, 255, 0])
+            led_stripe_service.pixels.fill([0, brightness, 0])
         elif gitlab_service.status == Status.FAILED:
             sleep_timer = 0.5
-            if led_stripe_service.pixels[0] == [255, 0, 0]:
+            if led_stripe_service.pixels[0] == [brightness, 0, 0]:
                 led_stripe_service.pixels.fill([0, 0, 0])
             else:
-                led_stripe_service.pixels.fill([255, 0, 0])
+                led_stripe_service.pixels.fill([brightness, 0, 0])
         else:
             sleep_timer = 10
-            led_stripe_service.pixels.fill([255, 255, 0])
+            led_stripe_service.pixels.fill([brightness, brightness, 0])
         time.sleep(sleep_timer)
 
 
